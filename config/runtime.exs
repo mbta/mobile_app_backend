b6f7b788e3065c85153665be8e0f2fb75fdb05c4
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-10-27 15:36:21 -0400

    feat: Configure API URL and key environment variables

--- config/runtime.exs
+++ config/runtime.exs
@@ -7,6 +7,10 @@ import Config
 # any compile-time configuration in here, as it won't be applied.
 # The block below contains prod specific runtime configuration.
 
+config :mobile_app_backend,
+  api_url: System.get_env("API_URL"),
+  api_key: System.get_env("API_KEY")
+
 # ## Using releases
 #
 # If you use `mix release`, you need to explicitly enable the server
