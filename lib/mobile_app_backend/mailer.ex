f4f2bf2f91ef20e2dd46eb137bb80dddcea1584a
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-10-27 15:42:48 -0400

    docs: Add moduledocs for credo

--- lib/mobile_app_backend/mailer.ex
+++ lib/mobile_app_backend/mailer.ex
@@ -1,3 +1,7 @@
 defmodule MobileAppBackend.Mailer do
+  @moduledoc """
+  Email support.
+  """
+
   use Swoosh.Mailer, otp_app: :mobile_app_backend
 end
--- lib/mobile_app_backend_web/components/layouts.ex
+++ lib/mobile_app_backend_web/components/layouts.ex
@@ -1,4 +1,8 @@
 defmodule MobileAppBackendWeb.Layouts do
+  @moduledoc """
+  Render page layouts.
+  """
+
   use MobileAppBackendWeb, :html
 
   embed_templates "layouts/*"
