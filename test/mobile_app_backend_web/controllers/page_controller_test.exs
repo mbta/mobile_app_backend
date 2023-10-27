601caf0f07ba5eec4a3bb53e9ad89a4f6bd8d476
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-10-27 15:01:05 -0400

    feat: Replace the default Phoenix landing page content

--- test/mobile_app_backend_web/controllers/page_controller_test.exs
+++ test/mobile_app_backend_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MobileAppBackendWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Mobile App Backend"
   end
 end
