478f940b1d7fa2edc32c89b3a4c110d3e641dbea
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2023-10-27 15:58:15 -0400

    feat: Health check endpoint

---
+++ lib/mobile_app_backend_web/controllers/health_controller.ex
@@ -0,0 +1,11 @@
+defmodule MobileAppBackendWeb.HealthController do
+  @moduledoc """
+  Simple controller to return 200 OK when the application is running.
+  This is used by the AWS ALB to determine the health of the target.
+  """
+  use MobileAppBackendWeb, :controller
+
+  def index(conn, _params) do
+    send_resp(conn, :ok, "Ok")
+  end
+end
