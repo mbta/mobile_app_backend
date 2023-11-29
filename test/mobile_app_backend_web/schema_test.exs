e1dbbc3380de3d55c0777af218d1151c19430e7b
Melody Horn <mhorn@mbta.com>
2023-11-29 16:10:15 -0700

    add GraphQL test

---
+++ test/mobile_app_backend_web/schema_test.exs
@@ -0,0 +1,47 @@
+defmodule MobileAppBackendWeb.SchemaTest do
+  use MobileAppBackendWeb.ConnCase
+
+  @stop_query """
+  query {
+    stop(id: "place-boyls") {
+      id
+      name
+      routes {
+        id
+        name
+        routePatterns {
+          id
+          name
+        }
+      }
+    }
+  }
+  """
+
+  test "query: stop", %{conn: conn} do
+    conn =
+      post(conn, "/graphql", %{
+        "query" => @stop_query
+      })
+
+    assert %{"data" => %{"stop" => stop_data}} = json_response(conn, 200)
+    assert %{"id" => "place-boyls", "name" => "Boylston", "routes" => routes} = stop_data
+
+    routes = Enum.sort_by(routes, & &1["id"])
+
+    assert routes |> Enum.map(& &1["id"]) == [
+             "Green-B",
+             "Green-C",
+             "Green-D",
+             "Green-E"
+           ]
+
+    assert %{
+             "id" => "Green-B",
+             "name" => "Green Line B",
+             "routePatterns" => route_patterns
+           } = hd(routes)
+
+    assert length(route_patterns) > 0
+  end
+end
