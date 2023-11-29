98fede84966b146e057478d97bc886caf2b6cd1b
Melody Horn <mhorn@mbta.com>
2023-11-29 14:29:02 -0700

    feat: bring in Stops.Repo from dotcom

--- lib/mobile_app_backend_web/resolvers/stop.ex
+++ lib/mobile_app_backend_web/resolvers/stop.ex
@@ -1,8 +1,5 @@
 defmodule MobileAppBackendWeb.Resolvers.Stop do
   def find_stop(_parent, %{id: id}, _resolution) do
-    case Stops.Api.by_gtfs_id(id) do
-      {:ok, stop} when not is_nil(stop) -> {:ok, stop}
-      x -> {:error, x}
-    end
+    {:ok, Stops.Repo.get!(id)}
   end
 end
