1edbdffee5d4248b36cc04deeab1604caa40dedb
Melody Horn <mhorn@mbta.com>
2024-01-22 10:17:28 -0700

    split up params tests

--- test/mbta_v3_api/json_api/params_test.exs
+++ test/mbta_v3_api/json_api/params_test.exs
@@ -5,22 +5,32 @@ defmodule MBTAV3API.JsonApi.ParamsTest do
   describe "flatten_params/1" do
     import MBTAV3API.JsonApi.Params, only: [flatten_params: 2]
 
-    test "sort works" do
+    test "does not sort if no sort given" do
       refute Map.has_key?(flatten_params([], :stop), "sort")
+    end
+
+    test "sorts ascending" do
       assert %{"sort" => "name"} = flatten_params([sort: {:name, :asc}], :trip)
+    end
+
+    test "sorts descending" do
       assert %{"sort" => "-id"} = flatten_params([sort: {:id, :desc}], :route)
     end
 
-    test "fields work" do
+    test "uses fields for type if no includes or overrides" do
       assert %{"fields[stop]" => "latitude,longitude,name"} = flatten_params([], :stop)
+    end
 
+    test "uses fields for included type" do
       assert %{
                "fields[route]" =>
                  "color,direction_names,direction_destinations,long_name,short_name,sort_order,text_color",
                "fields[route_pattern]" => "direction_id,name,sort_order"
              } =
                flatten_params([include: :route], :route_pattern)
+    end
 
+    test "can override fields" do
       assert %{
                "fields[route_pattern]" => "direction_id,name,sort_order",
                "fields[trip]" => "asdf,ghjk"
@@ -31,11 +41,15 @@ defmodule MBTAV3API.JsonApi.ParamsTest do
                )
     end
 
-    test "include works" do
+    test "does not include if no include given" do
       refute Map.has_key?(flatten_params([], :trip), "include")
+    end
 
+    test "includes single related object" do
       assert %{"include" => "parent_station"} = flatten_params([include: :parent_station], :stop)
+    end
 
+    test "handles nested includes" do
       assert %{"include" => "route,representative_trip,representative_trip.stops"} =
                flatten_params([include: [:route, representative_trip: :stops]], :route_pattern)
     end
