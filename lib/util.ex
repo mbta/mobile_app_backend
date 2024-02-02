f00f4c3f423e070a7218f83675ad5439ce88926f
Melody Horn <mhorn@mbta.com>
2024-02-02 10:47:51 -0700

    document and doctest Util.parse_optional_datetime/1

--- lib/util.ex
+++ lib/util.ex
@@ -1,4 +1,15 @@
 defmodule Util do
+  @doc """
+  Parses an optional value as an `America/New_York` datetime.
+
+  ## Examples
+
+      iex> Util.parse_optional_datetime(nil)
+      nil
+
+      iex> Util.parse_optional_datetime("2024-02-02T10:45:52-05:00")
+      #DateTime<2024-02-02 10:45:52-05:00 EST America/New_York>
+  """
   @spec parse_optional_datetime(String.t() | nil) :: DateTime.t() | nil
   def parse_optional_datetime(data)
   def parse_optional_datetime(nil), do: nil
