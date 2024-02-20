262b2fad93f28c823a54f9949fbd84007530290f
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-02-20 14:58:22 -0500

    fix(Repository.Behavior): correctly alias JsonAPI

--- lib/mbta_v3_api/repository/behavior.ex
+++ lib/mbta_v3_api/repository/behavior.ex
@@ -1,4 +1,6 @@
 defmodule MBTAV3API.Repository.Behaviour do
+  alias MBTAV3API.JsonApi
+
   @callback all_alerts(JsonApi.Params.t(), Keyword.t()) ::
               {:ok, [MBTAV3API.Alert.t()]} | {:error, term()}
 
