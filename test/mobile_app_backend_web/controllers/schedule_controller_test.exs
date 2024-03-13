76e4d59d37abd9f70b32ba98c05d2cb11e0ae74d
Melody Horn <mhorn@mbta.com>
2024-03-13 11:07:17 -0600

    test: use mock data instead of snapshot for schedules

--- test/mobile_app_backend_web/controllers/schedule_controller_test.exs
+++ test/mobile_app_backend_web/controllers/schedule_controller_test.exs
@@ -1,8 +1,57 @@
 defmodule MobileAppBackendWeb.ScheduleControllerTest do
   use MobileAppBackendWeb.ConnCase
   use HttpStub.Case
+  import Mox
+  import MobileAppBackend.Factory
+  import Test.Support.Helpers
+  import Test.Support.Sigils
+
+  setup do
+    verify_on_exit!()
+    reassign_env(:mobile_app_backend, MBTAV3API.Repository, RepositoryMock)
+    :ok
+  end
 
   test "returns schedules", %{conn: conn} do
+    s1 =
+      %MBTAV3API.Schedule{
+        id: "schedule-60565179-70159-90",
+        arrival_time: ~B[2024-03-13 01:07:00],
+        departure_time: ~B[2024-03-13 01:07:00],
+        drop_off_type: :regular,
+        pick_up_type: :regular,
+        stop_id: "70159",
+        trip_id: "60565179"
+      }
+
+    s2 = %MBTAV3API.Schedule{
+      id: "schedule-60565145-70158-590",
+      arrival_time: "2024-03-13T01:15:00-04:00",
+      departure_time: "2024-03-13T01:15:00-04:00",
+      drop_off_type: :regular,
+      pick_up_type: :regular,
+      stop_id: "70158",
+      trip_id: "60565145"
+    }
+
+    t1 = build(:trip, id: s1.trip_id)
+    t2 = build(:trip, id: s2.trip_id)
+
+    RepositoryMock
+    |> expect(:schedules, fn params, _opts ->
+      assert [
+               filter: [
+                 stop: "place-boyls",
+                 date: ~D[2024-03-12],
+                 min_time: "25:06"
+               ],
+               include: :trip,
+               sort: {:departure_time, :asc}
+             ] = params
+
+      ok_response([s1, s2], [t1, t2])
+    end)
+
     conn =
       get(conn, "/api/schedules", %{
         stop_ids: "place-boyls",
@@ -31,20 +80,8 @@ defmodule MobileAppBackendWeb.ScheduleControllerTest do
                }
              ],
              "trips" => %{
-               "60565145" => %{
-                 "headsign" => "Medford/Tufts",
-                 "id" => "60565145",
-                 "route_pattern_id" => "Green-E-886-1",
-                 "shape_id" => "8000015",
-                 "stop_ids" => nil
-               },
-               "60565179" => %{
-                 "headsign" => "Riverside",
-                 "id" => "60565179",
-                 "route_pattern_id" => "Green-D-855-0",
-                 "shape_id" => "8000008",
-                 "stop_ids" => nil
-               }
+               "60565145" => %{},
+               "60565179" => %{}
              }
            } = json_response(conn, 200)
   end
