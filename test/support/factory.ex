057aa3a1fac18b39555d6f5c1b6851399a6cad47
Melody Horn <mhorn@mbta.com>
2024-03-21 15:13:27 -0600

    fix: use random IDs for factory objects

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,7 +5,7 @@ defmodule MobileAppBackend.Factory do
 
   def alert_factory do
     %MBTAV3API.Alert{
-      id: "553702",
+      id: Uniq.UUID.uuid7(),
       active_period: [
         %MBTAV3API.Alert.ActivePeriod{
           start: ~B[2024-02-14T05:22:00],
@@ -26,7 +26,7 @@ defmodule MobileAppBackend.Factory do
 
   def prediction_factory do
     %MBTAV3API.Prediction{
-      id: "prediction-ADDED-1591593214-70206-20",
+      id: Uniq.UUID.uuid7(),
       arrival_time: nil,
       departure_time: ~B[2024-03-20 16:41:01],
       direction_id: 0,
@@ -43,7 +43,7 @@ defmodule MobileAppBackend.Factory do
 
   def route_pattern_factory do
     %MBTAV3API.RoutePattern{
-      id: "66-6-0",
+      id: Uniq.UUID.uuid7(),
       name: "Nubian Station - Harvard Square",
       direction_id: 0,
       sort_order: 506_600_000,
@@ -54,7 +54,7 @@ defmodule MobileAppBackend.Factory do
 
   def route_factory do
     %MBTAV3API.Route{
-      id: "66",
+      id: Uniq.UUID.uuid7(),
       long_name: "Harvard Square - Nubian Station",
       short_name: "66",
       type: :bus,
@@ -66,7 +66,7 @@ defmodule MobileAppBackend.Factory do
 
   def shape_factory do
     %MBTAV3API.Shape{
-      id: "660250",
+      id: Uniq.UUID.uuid7(),
       polyline:
         "injaG~qzpLAFCVK|A`Bt@\\XHLk@r@{@lA_@t@O\\??m@vAcAzB[|@Kj@Qf@Ql@Or@??S`Ac@|B[nCKnAAL??ElAG|BC`B?|CCrA??A^E`AOtBCh@Gx@MfA??APKpBEj@[pEQbBEh@c@lC??SpAQz@Ot@EXS~@i@rBu@jCER??St@Ux@o@dCm@fCg@vB??m@pCQp@h@pAh@z@Zr@??\\r@Pd@HZH`@B\\@t@@bDArC?ZBt@@l@??@RBl@Fv@Ll@HZNb@h@lAR`@Zl@tA|BJTHRCV@LBvAFpA??Bn@DdA@`BAxA?VAxB?xAFnARjC??@RGlAEXI^KXWh@ONYXURs@f@SPc@X_AAI@??kBVg@Hi@Lc@Py@\\_@P{@j@a@`@u@|@mAbBUZ??]d@KPQNc@b@m@\\]Jc@Hq@DaAEw@Cs@E??G?eAK{@SOGe@OoA_@YGkA]YCu@ByAZkBZqAZ??QDo@TKLg@|@k@hA[x@iBpDkB`E_@|@KXM\\KP??OXSj@s@nBcApC]t@k@hA_@j@s@t@GHGF??qA`Ae@^{@d@cB~@qBhA{BfAED??y@h@KD]V_@XEFw@z@cAz@_@V??UNYLMHSHMDaCjAy@\\qBh@iAVE@??iBZWDi@bJ??m@pJAr@Ap@Cp@C`AIzASi@]oAYw@??IWUu@Wy@u@_Cq@sBGS_@kAc@uA[}@k@eBc@gA????Me@_AcDs@cCUeBq@mCkBiHoBkHEUWLyAt@SH??YLs@`@IFm@v@SZmAbBq@|@o@z@??IL]d@aApAOVy@fA}@`Am@b@_@NWF??KBa@Am@I_@K]KOKWQa@[_@e@[]_@m@[g@??iDeFmAmB{BiDYc@Y[CGu@_AgAkAkAgAk@m@WU??w@w@iD}Cg@c@q@i@m@g@}@u@qB_BaCmBoAcAUQ}AgASO??QMiByAUQ{@w@wBaBw@a@Sk@a@a@g@WMIOGg@IOCu@UiBY????}@Oc@?g@F]Re@v@i@lA@NDPLRDF`Bm@t@[`@K"
     }
@@ -74,7 +74,7 @@ defmodule MobileAppBackend.Factory do
 
   def stop_factory do
     %MBTAV3API.Stop{
-      id: "22549",
+      id: Uniq.UUID.uuid7(),
       name: "Harvard Sq @ Garden St - Dawes Island",
       latitude: 42.375302,
       longitude: -71.119237,
@@ -84,14 +84,14 @@ defmodule MobileAppBackend.Factory do
 
   def trip_factory do
     %MBTAV3API.Trip{
-      id: "60168428",
+      id: Uniq.UUID.uuid7(),
       headsign: "Harvard via Allston"
     }
   end
 
   def vehicle_factory do
     %MBTAV3API.Vehicle{
-      id: "y1919",
+      id: Uniq.UUID.uuid7(),
       current_status: :in_transit_to,
       stop_id: "7670",
       trip_id: "60169926"
