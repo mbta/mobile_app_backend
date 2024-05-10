c60cfbd1b6f154c5cfa7b27a9aa690ffdcd4f252
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-05-10 09:46:19 -0400

    feat(Vehicle): Add fields

--- lib/mbta_v3_api/vehicle.ex
+++ lib/mbta_v3_api/vehicle.ex
@@ -9,6 +9,8 @@ defmodule MBTAV3API.Vehicle do
           direction_id: 0 | 1,
           latitude: float(),
           longitude: float(),
+          occupancy_status: occupancy_status(),
+          updated_at: DateTime.t(),
           route_id: String.t() | nil,
           stop_id: String.t() | nil,
           trip_id: String.t() | nil
@@ -16,6 +18,19 @@ defmodule MBTAV3API.Vehicle do
   Util.declare_enum(
     :current_status,
     Util.enum_values(:uppercase_string, [:incoming_at, :stopped_at, :in_transit_to])
+  )
+
+  Util.declare_enum(
+    :occupancy_status,
+    Util.enum_values(:uppercase_string, [
+      :many_seats_available,
+      :few_seats_available,
+      :standing_room_only,
+      :crushed_standing_room_only,
+      :full,
+      :not_accepting_passengers,
+      :no_data_available
+    ])
   )
 
   @derive Jason.Encoder
@@ -26,13 +41,24 @@ defmodule MBTAV3API.Vehicle do
     :direction_id,
     :latitude,
     :longitude,
+    :occupancy_status,
+    :updated_at,
     :route_id,
     :stop_id,
     :trip_id
   ]
 
   @impl JsonApi.Object
-  def fields, do: [:bearing, :current_status, :direction_id, :latitude, :longitude]
+  def fields,
+    do: [
+      :bearing,
+      :current_status,
+      :direction_id,
+      :latitude,
+      :longitude,
+      :occupancy_status,
+      :updated_at
+    ]
 
   @impl JsonApi.Object
   def includes, do: %{route: MBTAV3API.Route, stop: MBTAV3API.Stop, trip: MBTAV3API.Trip}
@@ -46,6 +72,8 @@ defmodule MBTAV3API.Vehicle do
       direction_id: item.attributes["direction_id"],
       latitude: item.attributes["latitude"],
       longitude: item.attributes["longitude"],
+      occupancy_status: parse_occupancy_status(item.attributes["occupancy_status"]),
+      updated_at: Util.parse_datetime!(item.attributes["updated_at"]),
       route_id: JsonApi.Object.get_one_id(item.relationships["route"]),
       stop_id: JsonApi.Object.get_one_id(item.relationships["stop"]),
       trip_id: JsonApi.Object.get_one_id(item.relationships["trip"])
--- test/mbta_v3_api/vehicle_test.exs
+++ test/mbta_v3_api/vehicle_test.exs
@@ -1,6 +1,7 @@
 defmodule MBTAV3API.VehicleTest do
   use ExUnit.Case, async: true
 
+  import Test.Support.Sigils
   alias MBTAV3API.JsonApi
   alias MBTAV3API.Vehicle
 
@@ -12,9 +13,11 @@ defmodule MBTAV3API.VehicleTest do
              direction_id: 0,
              latitude: 42.359901428222656,
              longitude: -71.09449005126953,
+             occupancy_status: :many_seats_available,
              route_id: "1",
              stop_id: "99",
-             trip_id: "61391720"
+             trip_id: "61391720",
+             updated_at: ~B[2024-01-24 17:08:51]
            } ==
              Vehicle.parse(%JsonApi.Item{
                type: "vehicle",
@@ -22,9 +25,11 @@ defmodule MBTAV3API.VehicleTest do
                attributes: %{
                  "bearing" => 315,
                  "current_status" => "IN_TRANSIT_TO",
+                 "occupancy_status" => "MANY_SEATS_AVAILABLE",
                  "direction_id" => 0,
                  "latitude" => 42.359901428222656,
-                 "longitude" => -71.09449005126953
+                 "longitude" => -71.09449005126953,
+                 "updated_at" => "2024-01-24T17:08:51-05:00"
                },
                relationships: %{
                  "route" => %JsonApi.Reference{type: "route", id: "1"},
