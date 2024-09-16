82db9366adf56b241ed6176b2e689e908a4da368
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-09-16 13:15:51 -0400

    WIP: try in-memory cache for V3 API

---
+++ lib/mbta_v3_api/response_cache.ex
@@ -0,0 +1,11 @@
+defmodule MBTAV3API.ResponseCache do
+  @moduledoc """
+  Cache used to reduce the number of duplicate API calls.
+  Responses are stored as tuple {last_updated_timestamp, response}
+  """
+  use Nebulex.Cache, otp_app: :mobile_app_backend, adapter: Nebulex.Adapters.Local
+
+  def cache_key(url, params) do
+    "#{url} params=#{inspect(params)}"
+  end
+end
