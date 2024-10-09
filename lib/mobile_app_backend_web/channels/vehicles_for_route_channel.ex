ec68bbcdc1dbf5e3840d6a96b696e3ec6dfa8735
Emma Simon <esimon@mbta.com>
2024-10-09 11:31:40 -0400

    feat: Add new multi-route vehicle channel

--- lib/mobile_app_backend_web/channels/vehicles_for_route_channel.ex
+++ lib/mobile_app_backend_web/channels/vehicles_for_route_channel.ex
@@ -15,12 +15,34 @@ defmodule MobileAppBackendWeb.VehiclesForRouteChannel do
 
       {:ok, vehicle_data} = MBTAV3API.Stream.StaticInstance.subscribe("vehicles")
 
-      vehicle_data = filter_data(vehicle_data, route_id, direction_id)
+      vehicle_data = filter_data(vehicle_data, [route_id], direction_id)
 
       {:ok, vehicle_data,
        assign(socket,
          data: vehicle_data,
-         route_id: route_id,
+         route_ids: [route_id],
+         direction_id: direction_id,
+         throttler: throttler
+       )}
+    else
+      :error -> {:error, %{code: :missing_param}}
+    end
+  end
+
+  @impl true
+  def join("vehicles:routes:" <> topic_param_concat, _payload, socket) do
+    with {:ok, route_ids, direction_id} <- parse_params(topic_param_concat) do
+      {:ok, throttler} =
+        MobileAppBackend.Throttler.start_link(target: self(), cast: :send_data, ms: @throttle_ms)
+
+      {:ok, vehicle_data} = MBTAV3API.Stream.StaticInstance.subscribe("vehicles")
+
+      vehicle_data = filter_data(vehicle_data, route_ids, direction_id)
+
+      {:ok, vehicle_data,
+       assign(socket,
+         data: vehicle_data,
+         route_ids: route_ids,
          direction_id: direction_id,
          throttler: throttler
        )}
@@ -32,7 +54,7 @@ defmodule MobileAppBackendWeb.VehiclesForRouteChannel do
   @impl true
   def handle_info({:stream_data, "vehicles", data}, socket) do
     old_data = socket.assigns.data
-    new_data = filter_data(data, socket.assigns.route_id, socket.assigns.direction_id)
+    new_data = filter_data(data, socket.assigns.route_ids, socket.assigns.direction_id)
 
     if old_data != new_data do
       MobileAppBackend.Throttler.request(socket.assigns.throttler)
@@ -48,11 +70,28 @@ defmodule MobileAppBackendWeb.VehiclesForRouteChannel do
     {:noreply, socket}
   end
 
-  @spec filter_data(JsonApi.Object.full_map(), String.t(), 0 | 1) :: JsonApi.Object.full_map()
-  defp filter_data(vehicle_data, route_id, direction_id) do
+  @spec parse_params(String.t()) :: {:ok, [String.t()], 0 | 1} | :error
+  defp parse_params(param_string) do
+    if param_string == "" || !String.contains?(param_string, ":") do
+      :error
+    else
+      [route_string, direction_string] = String.split(param_string, ":", parts: 2)
+
+      case Integer.parse(direction_string) do
+        {direction_id, _} when direction_id in [0, 1] ->
+          {:ok, String.split(route_string, ","), direction_id}
+
+        _ ->
+          :error
+      end
+    end
+  end
+
+  @spec filter_data(JsonApi.Object.full_map(), [String.t()], 0 | 1) :: JsonApi.Object.full_map()
+  defp filter_data(vehicle_data, route_ids, direction_id) do
     update_in(vehicle_data.vehicles, fn vehicles ->
       Map.filter(vehicles, fn {_, %Vehicle{} = vehicle} ->
-        vehicle.route_id == route_id and vehicle.direction_id == direction_id
+        Enum.member?(route_ids, vehicle.route_id) and vehicle.direction_id == direction_id
       end)
     end)
   end
