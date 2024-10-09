ee83b1d7d66c9a5a2f378b9fc2c36af14019d7e2
Emma Simon <esimon@mbta.com>
2024-10-09 13:20:18 -0400

    feat: Add new multi-route vehicles channel (#214)
    * fix: Add direnv to .tool-versions
    
    * feat: Add new multi-route vehicle channel
    
    * test: Add tests for multi route vehicles channel
    
    * fix: Clean up credo warnings

--- test/mobile_app_backend_web/channels/vehicles_for_route_channel_test.exs
+++ test/mobile_app_backend_web/channels/vehicles_for_route_channel_test.exs
@@ -67,4 +67,68 @@ defmodule MobileAppBackendWeb.VehiclesForRouteChannelTest do
     assert_push "stream_data", data
     assert data == to_full_map([good_vehicle1])
   end
+
+  test "joins multi route ok", %{socket: socket} do
+    route_ids = ["123", "456", "789"]
+    direction_id = 1
+    vehicle1 = build(:vehicle, route_id: Enum.at(route_ids, 0), direction_id: direction_id)
+    vehicle2 = build(:vehicle, route_id: Enum.at(route_ids, 1), direction_id: direction_id)
+    vehicle3 = build(:vehicle, route_id: Enum.at(route_ids, 2), direction_id: direction_id)
+
+    start_link_supervised!(
+      {FakeStaticInstance, topic: "vehicles", data: to_full_map([vehicle1, vehicle2, vehicle3])}
+    )
+
+    {:ok, reply, _socket} =
+      subscribe_and_join(
+        socket,
+        "vehicles:routes:#{Enum.join(route_ids, ",")}:#{direction_id}",
+        %{}
+      )
+
+    assert reply == to_full_map([vehicle1, vehicle2, vehicle3])
+  end
+
+  test "filters multi route to requested data", %{socket: socket} do
+    route_id_1 = "123"
+    route_id_2 = "456"
+    route_ids = [route_id_1, route_id_2]
+    direction_id = 0
+    good_vehicle1 = build(:vehicle, route_id: route_id_1, direction_id: direction_id)
+    good_vehicle2 = build(:vehicle, route_id: route_id_2, direction_id: direction_id)
+    bad_vehicle1 = build(:vehicle, route_id: "NOT-#{route_id_1}", direction_id: direction_id)
+    bad_vehicle2 = build(:vehicle, route_id: route_id_1, direction_id: 1 - direction_id)
+    bad_vehicle3 = build(:vehicle, route_id: "NOT-#{route_id_2}", direction_id: 1 - direction_id)
+
+    start_link_supervised!(
+      {FakeStaticInstance,
+       topic: "vehicles",
+       data: to_full_map([good_vehicle1, good_vehicle2, bad_vehicle1, bad_vehicle2, bad_vehicle3])}
+    )
+
+    {:ok, reply, _socket} =
+      subscribe_and_join(
+        socket,
+        "vehicles:routes:#{Enum.join(route_ids, ",")}:#{direction_id}",
+        %{}
+      )
+
+    assert reply == to_full_map([good_vehicle1, good_vehicle2])
+
+    Stream.PubSub.broadcast!(
+      "vehicles",
+      {:stream_data, "vehicles",
+       to_full_map([good_vehicle1, good_vehicle2, bad_vehicle1, bad_vehicle2])}
+    )
+
+    refute_push "stream_data", _
+
+    Stream.PubSub.broadcast!(
+      "vehicles",
+      {:stream_data, "vehicles", to_full_map([good_vehicle1, bad_vehicle2])}
+    )
+
+    assert_push "stream_data", data
+    assert data == to_full_map([good_vehicle1])
+  end
 end
