c1a83822575f31f67989d19418578ed802cea7d4
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2024-10-25 09:21:18 -0400

    fix(load_testing): bad find/replace of headers var

--- load_testing/locustfile.py
+++ load_testing/locustfile.py
@@ -128,7 +128,7 @@ class MobileAppUser(HttpUser, PhoenixChannelUser):
             self.stop_id = random.choice(all_stop_ids)
         predictions_for_stop = requests.get(
             "https://api-v3.mbta.com/predictions", 
-            params={"stop": self.stop_id}, v3_api_headers=self.v3_api_headers).json()["data"]
+            params={"stop": self.stop_id}, headers=self.v3_api_headers).json()["data"]
         if (len(predictions_for_stop) != 0):
             prediction = predictions_for_stop[0]
             trip_id = prediction["relationships"]["trip"]["data"]["id"]
