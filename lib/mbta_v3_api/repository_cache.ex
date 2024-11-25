1f01c6e2d7c5e952e2e784715b18731eed0859b5
Kayla Brady <31781298+KaylaBrady@users.noreply.github.com>
2024-11-25 13:28:10 -0500

    fix(RepositoryCache): Reduce cache key collisions (#247)
    * fix(RepositoryCache): Reduce cache key collisions
    
    * style: use RepositoryCache alias
    
    * refactor(RepositoryCache): implement KeyGenerator behavior directly

--- lib/mbta_v3_api/repository_cache.ex
+++ lib/mbta_v3_api/repository_cache.ex
@@ -2,5 +2,23 @@ defmodule MBTAV3API.RepositoryCache do
   @moduledoc """
   Cache used to reduce the number of calls to the V3 API.
   """
-  use Nebulex.Cache, otp_app: :mobile_app_backend, adapter: Nebulex.Adapters.Local
+  use Nebulex.Cache,
+    otp_app: :mobile_app_backend,
+    adapter: Nebulex.Adapters.Local,
+    default_key_generator: __MODULE__
+
+  @behaviour Nebulex.Caching.KeyGenerator
+
+  @impl Nebulex.Caching.KeyGenerator
+  def generate(mod, fun, []) do
+    "#{mod}|#{fun}"
+  end
+
+  def generate(mod, fun, [arg]) do
+    "#{mod}|#{fun}|#{:erlang.phash2(arg)}"
+  end
+
+  def generate(mod, fun, args) do
+    "#{mod}|#{fun}|#{:erlang.phash2(args)}"
+  end
 end
