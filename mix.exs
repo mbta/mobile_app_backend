f7aa57c772ed272f8cb64ddc380322aacf759788
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-03-04 09:26:24 -0500

    chore: cleanup unused plug_cowboy

--- mix.exs
+++ mix.exs
@@ -65,7 +65,6 @@ defmodule MobileAppBackend.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:plug_cowboy, "~> 2.5"},
       {:credo, "~> 1.7", only: [:dev, :test]},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_machina, "~> 2.8.0", only: :test},
