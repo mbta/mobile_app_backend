12a28b17d10e3ed38b849055957b72e47ee7a9bd
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2025-05-01 16:35:25 -0400

    fix: .exs extension for last_fresh_store_test

--- test/mobile_app_backend/health/checker/alerts/last_fresh_store_test.exs
+++ test/mobile_app_backend/health/checker/alerts/last_fresh_store_test.exs
@@ -1,21 +1,17 @@
 defmodule MobileAppBackend.Health.Checker.Alerts.LastFreshStoreTest do
-use ExUnit.Case
-alias MobileAppBackend.Health.Checker.Alerts
+  use ExUnit.Case
+  alias MobileAppBackend.Health.Checker.Alerts
 
-setup do
-        start_link_supervised!(Alerts.LastFreshStore)
-        :ok
-end
+  setup do
+    start_link_supervised!(Alerts.LastFreshStore)
+    :ok
+  end
 
   test "read and write last fresh timestamp" do
-
     Alerts.LastFreshStore.update_last_fresh_timestamp(~U[2024-01-01 00:00:00Z])
     assert ~U[2024-01-01 00:00:00Z] == Alerts.LastFreshStore.last_fresh_timestamp()
 
     Alerts.LastFreshStore.update_last_fresh_timestamp(~U[2024-01-02 00:00:00Z])
-    assert ~U[2024-01-02 00:00:00Z] == Alerts.LastFreshStore.last_fresh_timestamp
-
-
-
+    assert ~U[2024-01-02 00:00:00Z] == Alerts.LastFreshStore.last_fresh_timestamp()
   end
 end
