d92e42658eac528a32f27322071c77da90e5c85f
Emma Simon <esimon@mbta.com>
2025-05-09 16:55:33 -0400

    test: Add comma separated route test

--- test/mobile_app_backend_web/controllers/route_controller_test.exs
+++ test/mobile_app_backend_web/controllers/route_controller_test.exs
@@ -18,21 +18,25 @@ defmodule MobileAppBackendWeb.RouteControllerTest do
       north_quincy = build(:stop, id: "north_quincy")
 
       RepositoryMock
-      |> expect(:stops, 1, fn params, _opts ->
-        case params
-             |> Keyword.get(:filter) do
-          [route: "Red", direction_id: "0"] ->
-            ok_response([andrew, jfk, savin, north_quincy])
-        end
+      |> expect(:stops, 2, fn
+        [filter: [route: "Red", direction_id: "0"], fields: _], _opts ->
+          ok_response([andrew, jfk, savin, north_quincy])
+
+        [filter: [route: "Red", direction_id: "1"], fields: _], _opts ->
+          ok_response([north_quincy, savin, jfk, andrew])
       end)
+    end
+
+    defp mock_multi_route_data do
+      stop_a = build(:stop, id: "stopA")
+      stop_b = build(:stop, id: "stopB")
+      stop_c = build(:stop, id: "stopC")
+      stop_d = build(:stop, id: "stopD")
 
       RepositoryMock
-      |> expect(:stops, 1, fn params, _opts ->
-        case params
-             |> Keyword.get(:filter) do
-          [route: "Red", direction_id: "1"] ->
-            ok_response([north_quincy, savin, jfk, andrew])
-        end
+      |> expect(:stops, 1, fn
+        [filter: [route: "route1,route2", direction_id: "0"], fields: _], _opts ->
+          ok_response([stop_a, stop_b, stop_c, stop_d])
       end)
     end
 
@@ -53,6 +57,18 @@ defmodule MobileAppBackendWeb.RouteControllerTest do
       data1 = json_response(conn1, 200)
 
       assert %{"stop_ids" => ["north_quincy", "savin_hill", "jfk/umass", "andrew"]} = data1
+    end
+
+    test "joined list of stop IDs is returned when multiple routes are passed in",
+         %{conn: conn} do
+      mock_multi_route_data()
+
+      conn =
+        get(conn, "/api/route/stops", %{"route_id" => "route1,route2", "direction_id" => 0})
+
+      data = json_response(conn, 200)
+
+      assert %{"stop_ids" => ["stopA", "stopB", "stopC", "stopD"]} = data
     end
   end
 end
