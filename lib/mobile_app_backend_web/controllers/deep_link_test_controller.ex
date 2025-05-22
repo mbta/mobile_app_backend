80ad768282c897e0afe46355852905d05957cfc0
Melody Horn <mhorn@mbta.com>
2025-05-22 15:38:39 -0600

    correctly specify external vs internal redirect

--- lib/mobile_app_backend_web/controllers/deep_link_test_controller.ex
+++ lib/mobile_app_backend_web/controllers/deep_link_test_controller.ex
@@ -2,11 +2,11 @@ defmodule MobileAppBackendWeb.DeepLinkTestController do
   use MobileAppBackendWeb, :controller
 
   def redir_dotcom(conn, _params) do
-    redirect(conn, to: "https://dev-green.mbtace.com/go")
+    redirect(conn, external: "https://dev-green.mbtace.com/go")
   end
 
   def redir_backend(conn, _params) do
-    redirect(conn, external: "/go")
+    redirect(conn, to: "/go")
   end
 
   def link_dotcom(conn, _params) do
