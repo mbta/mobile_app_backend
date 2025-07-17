40ff79956442e3a4349e92a5bb7dacc2bde042b4
Melody Horn <mhorn@mbta.com>
2025-07-17 15:13:10 -0600

    test Providence/Stoughton Line via API with test data instead

--- test/mobile_app_backend_web/controllers/route_controller_test.exs
+++ test/mobile_app_backend_web/controllers/route_controller_test.exs
@@ -71,4 +71,155 @@ defmodule MobileAppBackendWeb.RouteControllerTest do
       assert %{"stop_ids" => ["stopA", "stopB", "stopC", "stopD"]} = data
     end
   end
+
+  describe "GET /api/route/stop-graph integration test" do
+    setup do
+      Mox.stub_with(MobileAppBackend.HTTPMock, Test.Support.HTTPStub)
+      :ok
+    end
+
+    test "returns the correct list for the Providence/Stoughton Line outbound", %{conn: conn} do
+      conn =
+        get(conn, "/api/route/stop-graph", %{"route_id" => "CR-Providence", "direction_id" => 0})
+
+      data = json_response(conn, 200)
+
+      empty = %{
+        "before" => false,
+        "converging" => false,
+        "current_stop" => false,
+        "diverging" => false,
+        "after" => false
+      }
+
+      forward = %{
+        "before" => true,
+        "converging" => false,
+        "current_stop" => true,
+        "diverging" => false,
+        "after" => true
+      }
+
+      skip = %{forward | "current_stop" => false}
+
+      assert data == [
+               %{
+                 "name" => nil,
+                 "stops" => [
+                   %{
+                     "stick_state" => %{
+                       "left" => empty,
+                       "right" => %{forward | "before" => false}
+                     },
+                     "stop_id" => "place-sstat"
+                   },
+                   %{
+                     "stick_state" => %{"left" => empty, "right" => forward},
+                     "stop_id" => "place-bbsta"
+                   },
+                   %{
+                     "stick_state" => %{"left" => empty, "right" => forward},
+                     "stop_id" => "place-rugg"
+                   }
+                 ],
+                 "typical?" => true
+               },
+               %{
+                 "name" => nil,
+                 "stops" => [
+                   %{
+                     "stick_state" => %{"left" => empty, "right" => forward},
+                     "stop_id" => "place-NEC-2203"
+                   },
+                   %{
+                     "stick_state" => %{"left" => empty, "right" => forward},
+                     "stop_id" => "place-DB-0095"
+                   }
+                 ],
+                 "typical?" => false
+               },
+               %{
+                 "name" => nil,
+                 "stops" => [
+                   %{
+                     "stick_state" => %{"left" => empty, "right" => forward},
+                     "stop_id" => "place-NEC-2173"
+                   },
+                   %{
+                     "stick_state" => %{
+                       "left" => %{skip | "before" => false, "diverging" => true},
+                       "right" => %{forward | "diverging" => true}
+                     },
+                     "stop_id" => "place-NEC-2139"
+                   }
+                 ],
+                 "typical?" => true
+               },
+               %{
+                 "name" => "Stoughton",
+                 "stops" => [
+                   %{
+                     "stick_state" => %{"left" => skip, "right" => forward},
+                     "stop_id" => "place-SB-0156"
+                   },
+                   %{
+                     "stick_state" => %{"left" => skip, "right" => %{forward | "after" => false}},
+                     "stop_id" => "place-SB-0189"
+                   }
+                 ],
+                 "typical?" => true
+               },
+               %{
+                 "name" => nil,
+                 "stops" => [
+                   %{
+                     "stick_state" => %{"left" => forward, "right" => empty},
+                     "stop_id" => "place-NEC-2108"
+                   },
+                   %{
+                     "stick_state" => %{"left" => forward, "right" => empty},
+                     "stop_id" => "place-NEC-2040"
+                   },
+                   %{
+                     "stick_state" => %{"left" => forward, "right" => empty},
+                     "stop_id" => "place-NEC-1969"
+                   }
+                 ],
+                 "typical?" => true
+               },
+               %{
+                 "name" => nil,
+                 "stops" => [
+                   %{
+                     "stick_state" => %{"left" => forward, "right" => empty},
+                     "stop_id" => "place-NEC-1919"
+                   }
+                 ],
+                 "typical?" => false
+               },
+               %{
+                 "name" => "Providence",
+                 "stops" => [
+                   %{
+                     "stick_state" => %{"left" => forward, "right" => empty},
+                     "stop_id" => "place-NEC-1891"
+                   },
+                   %{
+                     "stick_state" => %{"left" => forward, "right" => empty},
+                     "stop_id" => "place-NEC-1851"
+                   },
+                   %{
+                     "stick_state" => %{"left" => forward, "right" => empty},
+                     "stop_id" => "place-NEC-1768"
+                   },
+                   %{
+                     "stick_state" => %{"left" => %{forward | "after" => false}, "right" => empty},
+                     "stop_id" => "place-NEC-1659"
+                   }
+                 ],
+                 "typical?" => true
+               }
+             ]
+    end
+  end
 end
