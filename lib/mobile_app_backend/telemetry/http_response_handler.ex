a64b188bee453e1a9d68c2b0b0326f80ba58bc6b
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2026-03-17 16:07:32 -0400

    feat(Telemetry.HttpResponseHandler): Add some socket telemetry too

--- lib/mobile_app_backend/telemetry/http_response_handler.ex
+++ lib/mobile_app_backend/telemetry/http_response_handler.ex
@@ -2,9 +2,9 @@ defmodule MobileAppBackend.Telemetry.HttpResponseHandler do
   require Logger
 
   def attach do
-    :telemetry.attach(
+    :telemetry.attach_many(
       "http-telemetry",
-      [:bandit, :request, :stop],
+      [[:bandit, :request, :stop], [:bandit, :websocket, :start], [:bandit, :websocket, :stop]],
       &__MODULE__.handle_event/4,
       nil
     )
@@ -16,6 +16,22 @@ defmodule MobileAppBackend.Telemetry.HttpResponseHandler do
     status=#{metadata.conn.status} \
     size=#{Map.get(measurements, :resp_body_bytes, nil)} \
     uncompressed_size=#{Map.get(measurements, :resp_uncompressed_body_bytes, nil)} \
+    duration_ms=#{System.convert_time_unit(measurements.duration, :native, :millisecond)}
+    """)
+  end
+
+  def handle_event([:bandit, :websocket, :start], measurements, metadata, _config) do
+    Logger.debug("""
+    socket_connection_opened \
+    compression_enabled=#{!is_nil(measurements.compress)} \
+    """)
+  end
+
+  def handle_event([:bandit, :websocket, :stop], measurements, metadata, _config) do
+    Logger.info("""
+    socket_connection_closed \
+    send_text_frame_bytes=#{measurements.send_text_frame_bytes} \
+    send_text_frame_count=#{measurements.send_text_frame_count} \
     duration_ms=#{System.convert_time_unit(measurements.duration, :native, :millisecond)}
     """)
   end
