3b07e432fc65b174e38c7cefee75e4607fe8cc01
KaylaBrady <31781298+KaylaBrady@users.noreply.github.com>
2026-04-17 15:53:23 -0400

    fix(ObanLogger): Simplify log

--- lib/mobile_app_backend/oban_logger.ex
+++ lib/mobile_app_backend/oban_logger.ex
@@ -2,22 +2,8 @@ defmodule ObanLogger do
   require Logger
 
   def handle_event([:oban, :job, :exception], _, meta, nil) do
-    log_data = %{
-      worker: meta.worker,
-      id: meta.id,
-      meta: meta.meta,
-      state: meta.state,
-      max_attempts: meta.max_attempts,
-      queue: meta.queue,
-      source: meta.source,
-      event: meta.event,
-      duration: meta.duration,
-      attempt: meta.attempt,
-      queue_time: meta.queue_time,
-      error: meta.error,
-      stacktrace: Exception.format_stacktrace(meta.stacktrace)
-    }
-
-    Logger.error(Jason.encode!(log_data))
+    Logger.error(
+      "#{__MODULE__} job:exception id=#{meta.id} error=#{inspect(error)} stack_trace=#{inspect(Exception.format_stacktrace(meta.stacktrace))}"
+    )
   end
 end
