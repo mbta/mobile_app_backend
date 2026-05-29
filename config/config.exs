f7f757b728ad69cfb48f1b89d1185629ba21a01a
Kayla Brady <kbrady@mbta.com>
2026-05-29 16:27:44 -0400

    feat(Notifications.StatsReporter): Simple stats cron job

--- config/config.exs
+++ config/config.exs
@@ -63,6 +63,7 @@ config :mobile_app_backend, Oban,
     {Oban.Plugins.Cron,
      crontab: [
        {"@daily", MobileAppBackend.Notifications.DeliveredNotificationPruner},
+       {"* * * * *", MobileAppBackend.Notifications.StatsReporter}, # TODO: Set to daily after testing in dev-orange
        {"* * * * *", MobileAppBackend.Notifications.Scheduler}
      ],
      timezone: "America/New_York"},
---
+++ lib/mobile_app_backend/notifications/stats_reporter.ex
@@ -0,0 +1,34 @@
+defmodule MobileAppBackend.Notifications.StatsReporter do
+  @moduledoc """
+  Periodically log stats about notifications usage
+  """
+  use Oban.Worker, max_attempts: 10
+  import Ecto.Query
+  require Logger
+  alias MBTAV3API.Store
+  alias MobileAppBackend.Notifications.{DeliveredNotification, Subscription}
+  alias MobileAppBackend.Repo
+  alias MobileAppBackend.User
+
+  @impl Oban.Worker
+  def perform(%Oban.Job{}) do
+
+    global = GlobalDataCache.get_data()
+
+    counts_by_route = Repo.all(from s in Subscription, group_by: [s.route_id], select: {s.route_id, count(s.id)})
+    count_subscriptions_by_user = Repo.all(from s in Subscription, group_by: [s.user_id], select: count(s.id))
+    user_count = length(count_subscriptions_by_user)
+
+    Enum.each(counts_by_route, fn {route_id, count} ->
+
+      mode = Map.get(global.routes, route_id, %{type: :unknown}).type
+
+      Logger.info("#{__MODULE__} counts_by_route route_id=#{route_id} mode=#{mode} count=#{count}")
+    end)
+
+    Logger.info("#{__MODULE__} subscriptions_per_user user_count=#{user_count} \
+    avg=#{Enum.sum(count_subscriptions_by_user) / user_count} \
+    max=#{Enum.max(count_subscriptions_by_user)}")
+  end
+
+end
