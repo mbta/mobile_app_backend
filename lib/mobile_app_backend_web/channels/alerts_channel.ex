48dbadae980657af9b84973bbbce6d42f375050b
Emma Simon <esimon@mbta.com>
2026-06-29 11:20:35 -0400

    feat: Diff alerts:v3 channel updates (#565)
    * feat: Send v3 alert stream diffs
    
    This uses socket assigns to remember the hashes of the previously sent
    alerts, which are then compared against incoming alerts. All alerts are
    always sent on join, then subsequent messages are sent as a list of IDs
    to remove, and a map of alert data to update the client with.
    
    * test: Add and update v3 alert channel tests

--- lib/mobile_app_backend_web/channels/alerts_channel.ex
+++ lib/mobile_app_backend_web/channels/alerts_channel.ex
@@ -1,8 +1,23 @@
 defmodule MobileAppBackendWeb.AlertsChannel do
   use MobileAppBackendWeb, :channel
+  alias MBTAV3API.Alert
+  alias MobileAppBackend.Alerts.AlertWithSummaries
+
+  defmodule AlertUpdate do
+    @type t :: %__MODULE__{
+            remove: [String.t()],
+            update: %{String.t() => AlertWithSummaries.t()}
+          }
+    @derive Jason.Encoder
+    defstruct [:remove, :update]
+
+    def empty?(update) do
+      Enum.empty?(update.remove) and Enum.empty?(update.update)
+    end
+  end
 
   @impl true
-  def join("alerts" = topic, _payload, socket) do
+  def join("alerts", _payload, socket) do
     pubsub_module =
       Application.get_env(
         :mobile_app_backend,
@@ -10,12 +25,12 @@ defmodule MobileAppBackendWeb.AlertsChannel do
         MobileAppBackend.Alerts.PubSub
       )
 
-    data = pubsub_module.subscribe(get_opts(topic, socket))
+    data = pubsub_module.subscribe(get_opts(socket))
     {:ok, data, socket}
   end
 
   @impl true
-  def join("alerts:v2" = topic, _payload, socket) do
+  def join("alerts:v2", _payload, socket) do
     pubsub_module =
       Application.get_env(
         :mobile_app_backend,
@@ -23,12 +38,12 @@ defmodule MobileAppBackendWeb.AlertsChannel do
         MobileAppBackend.Alerts.PubSub
       )
 
-    data = pubsub_module.subscribe(get_opts(topic, socket))
+    data = pubsub_module.subscribe(get_opts(socket))
     {:ok, data, socket}
   end
 
   @impl true
-  def join("alerts:v3" = topic, _payload, socket) do
+  def join("alerts:v3", _payload, socket) do
     pubsub_module =
       Application.get_env(
         :mobile_app_backend,
@@ -36,12 +51,14 @@ defmodule MobileAppBackendWeb.AlertsChannel do
         MobileAppBackend.Alerts.PubSub
       )
 
-    data = pubsub_module.subscribe(get_opts(topic, socket))
-    {:ok, data, socket}
+    %{alerts: alerts} = pubsub_module.subscribe(get_opts(socket))
+    socket = assign(socket, :last_alerts, alert_hashes(alerts))
+
+    {:ok, %AlertUpdate{remove: [], update: alerts}, socket}
   end
 
-  defp get_opts(topic, socket) do
-    case topic do
+  defp get_opts(socket) do
+    case socket.topic do
       "alerts" ->
         [legacy_compatibility: true, include_summaries: false]
 
@@ -61,7 +78,72 @@ defmodule MobileAppBackendWeb.AlertsChannel do
 
   @impl true
   def handle_info({:new_alerts, data}, socket) do
-    :ok = push(socket, "stream_data", data)
-    {:noreply, socket}
+    case socket.topic do
+      "alerts:v3" ->
+        {:noreply, handle_info_v3({:new_alerts, data}, socket)}
+
+      _ ->
+        :ok = push(socket, "stream_data", data)
+        {:noreply, socket}
+    end
+  end
+
+  defp handle_info_v3({:new_alerts, %{alerts: alerts}}, socket) do
+    current_hashes = alert_hashes(alerts)
+    old_hashes = socket.assigns.last_alerts
+
+    current_ids = MapSet.new(Map.keys(current_hashes))
+    old_ids = MapSet.new(Map.keys(old_hashes))
+
+    removed_ids = MapSet.difference(old_ids, current_ids)
+    new_ids = MapSet.difference(current_ids, old_ids)
+
+    update_ids =
+      current_ids
+      |> MapSet.intersection(old_ids)
+      |> MapSet.filter(fn id -> Map.get(current_hashes, id) != Map.get(old_hashes, id) end)
+      |> MapSet.union(new_ids)
+
+    response =
+      %AlertUpdate{
+        remove: MapSet.to_list(removed_ids),
+        update: Map.filter(alerts, fn {id, _alert} -> Enum.member?(update_ids, id) end)
+      }
+
+    if !AlertUpdate.empty?(response) do
+      :ok = push(socket, "stream_data", response)
+    end
+
+    assign(socket, :last_alerts, current_hashes)
+  end
+
+  @spec alert_hashes(%{String.t() => Alert.t()}) :: %{String.t() => String.t()}
+  defp alert_hashes(alert_map) do
+    Map.new(alert_map, fn {key, val} ->
+      {key,
+       val
+       |> Map.from_struct()
+       |> Enum.sort_by(fn {key, _value} -> key end)
+       |> Enum.map(fn {key, value} ->
+         case key do
+           # Do some hacky sorting of informed entities, since the backend can return them arbitrarily in any order
+           :informed_entity ->
+             {key,
+              Enum.sort_by(value, fn entity ->
+                "#{entity.route}-#{entity.stop}-#{entity.direction_id}-#{entity.route_type}-#{entity.facility}-#{entity.trip}"
+              end)}
+
+           # Summaries can also change order based on the entity order, so sort those too
+           :summaries ->
+             {key, Enum.sort_by(value, fn summary -> summary.summary end)}
+
+           _ ->
+             {key, value}
+         end
+       end)
+       |> :erlang.term_to_binary()
+       |> then(&:crypto.hash(:md5, &1))
+       |> Base.encode16()}
+    end)
   end
 end
