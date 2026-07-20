ee2e04bd5a86daacd78a78305d874f99b982c7cd
Kayla Brady <kbrady@mbta.com>
2026-07-20 13:10:35 -0400

    Revert "chore(deps): update dependency elixir to v1.20.2 (#595)"
    This reverts commit bfd7bc6c55a5e4878ec24f601a34f6f3935c1856.

--- lib/mbta_v3_api/store/vehicles.ex
+++ lib/mbta_v3_api/store/vehicles.ex
@@ -1,6 +1,7 @@
 defmodule MBTAV3API.Store.Vehicles do
   use GenServer
   use MBTAV3API.Store, implementation_module: MBTAV3API.Store.Vehicles.Impl
+  require Logger
   alias MBTAV3API.Vehicle
 end
 
@@ -10,6 +11,7 @@ defmodule MBTAV3API.Store.Vehicles.Impl do
   and can be read in parallel by other processes.
   """
   use GenServer
+  require Logger
   alias MBTAV3API.JsonApi
   alias MBTAV3API.Store
   alias MBTAV3API.Vehicle
