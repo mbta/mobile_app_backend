51bb9f37a3ec8f3ac318999596ec7d48d7f90e6c
Kayla Brady <kbrady@mbta.com>
2026-08-21 14:06:30 -0400

    fix(Notifications.Engine): Don't return update if user already received update

--- test/mobile_app_backend/notifications/engine_test.exs
+++ test/mobile_app_backend/notifications/engine_test.exs
@@ -349,6 +349,57 @@ defmodule MobileAppBackend.Notifications.EngineTest do
              Engine.notifications([subscription], [alert], now)
   end
 
+  test "sends no update if already received update" do
+    now = DateTime.now!("America/New_York")
+    upstream_timestamp = DateTime.add(now, -2)
+
+    alert =
+      build(:alert,
+        active_period: [%Alert.ActivePeriod{start: DateTime.add(now, -1), end: nil}],
+        effect: :suspension,
+        informed_entity: [%Alert.InformedEntity{activities: [:board], route: "Red"}],
+        last_push_notification_timestamp: upstream_timestamp
+      )
+
+    user = NotificationsFactory.insert(:user)
+
+    subscription =
+      NotificationsFactory.build(:notification_subscription,
+        user_id: user.id,
+        route_id: "Red",
+        stop_id: "place-sstat",
+        windows: [
+          NotificationsFactory.build(:window,
+            start_time: now |> DateTime.add(-1) |> DateTime.to_time(),
+            end_time: now |> DateTime.add(1) |> DateTime.to_time(),
+            days_of_week: Range.to_list(0..6)
+          )
+        ]
+      )
+
+    Repo.insert!(%DeliveredNotification{
+      user_id: user.id,
+      alert_id: alert.id,
+      upstream_timestamp:
+        alert.last_push_notification_timestamp
+        |> DateTime.add(-1, :minute)
+        |> DateTime.shift_zone!("Etc/UTC")
+        |> DateTime.truncate(:second)
+    })
+
+    Repo.insert!(%DeliveredNotification{
+      user_id: user.id,
+      alert_id: alert.id,
+      upstream_timestamp:
+        alert.last_push_notification_timestamp
+        |> DateTime.shift_zone!("Etc/UTC")
+        |> DateTime.truncate(:second)
+    })
+
+    assert [] =
+             Engine.notifications([subscription], [alert], now)
+  end
+
   test "sends notification with timestamp if open" do
     now = DateTime.now!("America/New_York")
     start_time = DateTime.add(now, -1)
