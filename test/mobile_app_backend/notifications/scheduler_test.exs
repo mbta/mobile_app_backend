cfacbc8f4204606f292668ec19123e31b424e041
Tony Lizarraga <antonio_lizarraga@outlook.com>
2026-09-07 08:42:04 -0400

    Add module to all logs

--- test/mobile_app_backend/notifications/scheduler_test.exs
+++ test/mobile_app_backend/notifications/scheduler_test.exs
@@ -73,69 +73,6 @@ defmodule MobileAppBackend.Notifications.SchedulerTest do
     )
   end
 
-  test "schedules notification in user's locale" do
-    now = DateTime.now!("America/New_York")
-
-    alert =
-      build(:alert,
-        active_period: [
-          %MBTAV3API.Alert.ActivePeriod{
-            start: DateTime.add(now, -48, :hour)
-          }
-        ],
-        effect: :suspension,
-        informed_entity: [
-          %MBTAV3API.Alert.InformedEntity{
-            activities: [:board, :exit, :ride],
-            route: "66",
-            route_type: :bus
-          }
-        ],
-        last_push_notification_timestamp: DateTime.add(now, -1, :minute)
-      )
-
-    user = NotificationsFactory.insert(:user, locale: "es")
-
-    NotificationsFactory.insert(:notification_subscription,
-      user_id: user.id,
-      route_id: "66",
-      stop_id: "1",
-      direction_id: 0,
-      windows: [
-        NotificationsFactory.build(:window,
-          start_time: now |> DateTime.add(-10, :minute) |> DateTime.to_time(),
-          end_time: now |> DateTime.add(10, :minute) |> DateTime.to_time(),
-          days_of_week: [Date.day_of_week(now)]
-        )
-      ]
-    )
-
-    start_link_supervised!(Store.Alerts)
-    Store.Alerts.process_reset([alert], [])
-    set_log_level(:info)
-
-    {{:ok, _}, log} =
-      with_log([level: :info], fn ->
-        perform_job(MobileAppBackend.Notifications.Scheduler, %{})
-      end)
-
-    assert log =~ "find_new_notifications_for_user status=ok"
-
-    assert_enqueued(
-      worker: Notifications.Deliverer,
-      args: %{
-        "user_id" => user.id,
-        "alert_id" => alert.id,
-        "title" => "66 autobús",
-        "body" => "Servicio suspendido hasta nuevo aviso",
-        "deep_link_path" => "/s/1/r/66/d/0",
-        "upstream_timestamp" => alert.last_push_notification_timestamp,
-        "type" => "notification",
-        "analytics_label" => "route=66;effect=suspension;type=notification"
-      }
-    )
-  end
-
   test "does not send duplicate notifications" do
     now = DateTime.now!("America/New_York")
 
